da08c09d5b18944dc16b64281855cb6c083d0ea3
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2022-08-09 23:03:30 +0300

    Create members.js

---
+++ members.js
@@ -0,0 +1,37 @@
+members = [
+  {
+    id: 1,
+    firstName: "Aziz",
+    lastName: "AlSaffar",
+    currentlyBorrowedBooks: [3, 9],
+    membership: "gold", // can borrow 3 books
+  },
+  {
+    id: 2,
+    firstName: "Hashim",
+    lastName: "Behbehani",
+    currentlyBorrowedBooks: [5],
+    membership: "gold", // can borrow 3 books
+  },
+  {
+    id: 3,
+    firstName: "Alhamza",
+    lastName: "Abdulmonem",
+    currentlyBorrowedBooks: [1, 2],
+    membership: "silver", // can borrow 2 books
+  },
+  {
+    id: 4,
+    firstName: "Reem",
+    lastName: "AlHasawi",
+    currentlyBorrowedBooks: [],
+    membership: "platinum", // can borrow 5 books
+  },
+  {
+    id: 5,
+    firstName: "Laila",
+    lastName: "AlKandery",
+    currentlyBorrowedBooks: [10, 7, 8],
+    membership: "platinum", // can borrow 5 books
+  },
+];
